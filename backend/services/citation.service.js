8397a1d66f64aa90a3105716433585fab420f9da
Sayesha140 <enaya871@gmail.com>
2026-01-17 20:57:53 +0600

    modified citation.service.js

--- backend/services/citation.service.js
+++ backend/services/citation.service.js
@@ -1,69 +1,7 @@
-// const axios = require('axios');
-// const { Cite, plugins } = require('@citation-js/core');
-// require('@citation-js/plugin-bibtex');
-// require('@citation-js/plugin-csl');
-// const { tidy } = require('bibtex-tidy');
-
-// // Helper to fetch and register a style from the web
-// const registerRemoteStyle = async (name, url) => {
-//     const config = plugins.config.get('@csl');
-//     if (!config.templates.has(name)) {
-//         const { data } = await axios.get(url);
-//         config.templates.add(name, data);
-//     }
-// };
-
-// // Start-up function to prepare styles
-// const initStyles = async () => {
-//     await registerRemoteStyle('ieee', 'https://raw.githubusercontent.com/citation-style-language/styles/master/ieee.csl');
-//     await registerRemoteStyle('mla', 'https://raw.githubusercontent.com/citation-style-language/styles/master/modern-language-association.csl');
-// };
-
-// // Run this once when the server starts
-// initStyles().catch(err => console.error("Styles failed to load", err));
-
-// const fetchAndFormatCitation = async (paperId) => {
-//     // 1. FETCH FROM SEMANTIC SCHOLAR
-//     const response = await axios.get(
-//         `https://api.semanticscholar.org/graph/v1/paper/${paperId}?fields=citationStyles`,
-//         { headers: { 'x-api-key': process.env.SEMANTIC_SCHOLAR_API_KEY } }
-//     );
-//     const rawBibtex = response.data.citationStyles.bibtex;
-
-//     // 2. CLEANING (This is the bibtex-tidy step)
-//     const cleaned = tidy(rawBibtex, { 
-//     curly: true,               // Enforce braces around all values
-//     numeric: true,             // Keep years as numbers
-//     stripEnclosingBraces: true,
-//     dropOptionalTitles: true,
-//     cleanEnclosingBraces: true,
-//     space: 2,                  // Use 2 spaces for indentation
-//     align: 13
-//     }).bibtex;
-
-//     // 3. FORMATTING
-//     const cite = new Cite(cleaned);
-    
-//     // Return array for easy .map() on React frontend
-//     return [
-//         { id: 'bibtex', label: 'BibTeX', html: cleaned, format: 'text' },
-//         { id: 'apa', label: 'APA', html: cite.format('bibliography', { template: 'apa', format: 'html' }) },
-//         { id: 'vancouver', label: 'Vancouver', html: cite.format('bibliography', { template: 'vancouver', format: 'html' }) },
-//         { id: 'ieee', label: 'IEEE', html: cite.format('bibliography', { template: 'ieee', format: 'html' }) },
-//         { id: 'mla', label: 'MLA', html: cite.format('bibliography', { template: 'mla', format: 'html' }) }
-//     ];
-// };
-
-// module.exports = { fetchAndFormatCitation };
-
-
-
-
-
-
-
 const axios = require('axios');
 const { Cite, plugins } = require('@citation-js/core');
+const fs = require('fs'); 
+const path = require('path');
 require('@citation-js/plugin-bibtex');
 require('@citation-js/plugin-csl');
 const { tidy } = require('bibtex-tidy');
@@ -72,23 +10,36 @@ const he = require('he');
 
 // Register remote CSL styles (non-default)
 
-const registerRemoteStyle = async (name, url) => {
+// const registerRemoteStyle = async (name, url) => {
+//   const config = plugins.config.get('@csl');
+//   if (!config.templates.has(name)) {
+//     const { data } = await axios.get(url);
+//     config.templates.add(name, data);
+//   }
+// };
+
+// const initStyles = async () => {
+//   await registerRemoteStyle(
+//     'ieee',
+//     'https://raw.githubusercontent.com/citation-style-language/styles/master/ieee.csl'
+//   );
+//   await registerRemoteStyle(
+//     'mla',
+//     'https://raw.githubusercontent.com/citation-style-language/styles/master/modern-language-association.csl'
+//   );
+// };
+
+const registerLocalStyle = async (name, filePath) => {
   const config = plugins.config.get('@csl');
   if (!config.templates.has(name)) {
-    const { data } = await axios.get(url);
-    config.templates.add(name, data);
+    const data = fs.readFileSync(filePath, 'utf8'); // read local CSL
+    config.templates.add(name, data); // register in citation-js
   }
 };
 
 const initStyles = async () => {
-  await registerRemoteStyle(
-    'ieee',
-    'https://raw.githubusercontent.com/citation-style-language/styles/master/ieee.csl'
-  );
-  await registerRemoteStyle(
-    'mla',
-    'https://raw.githubusercontent.com/citation-style-language/styles/master/modern-language-association.csl'
-  );
+  await registerLocalStyle('ieee', path.join(__dirname, '../styles/ieee.csl'));
+  await registerLocalStyle('mla', path.join(__dirname, '../styles/mla.csl'));
 };
 
 // Ensure styles load once during server startup
