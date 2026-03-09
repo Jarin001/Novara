f699a961bd4b5665b34e35544aa4a0df1871f8c1
Sayesha140 <enaya871@gmail.com>
2026-03-09 22:39:50 +0600

    fixed askAI controller

--- backend/controllers/paperAi.controller.js
+++ backend/controllers/paperAi.controller.js
@@ -1,29 +1,3 @@
-// const { askQuestionFromPaper } = require("../services/askAI.service");
-
-
-// exports.askPaperQuestion = async (req, res) => {
-//   try {
-//     const { pdfUrl, question } = req.body;
-//     const user = req.user;
-//     const token = req.headers.authorization?.split(' ')[1];
-
-//     if (!pdfUrl || !question) {
-//       return res.status(400).json({
-//         error: "Both pdfUrl and question are required in the request body",
-//       });
-//     }
-
-//     console.log(`[Controller] askPaperQuestion called for user: ${user?.email}, PDF: ${pdfUrl.substring(0, 100)}`);
-
-//     const answer = await askQuestionFromPaper(pdfUrl, question, user, token);
-
-//     res.json({ answer });
-//   } catch (err) {
-//     console.error("Error in askPaperQuestion:", err.message || err);
-//     res.status(500).json({ error: err.message || "Internal server error" });
-//   }
-// };
-
 const { askQuestionFromPaper } = require("../services/askAI.service");
 
 
